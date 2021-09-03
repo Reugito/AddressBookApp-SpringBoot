240c791e572f66b33988133344d7900b94d102f2
Reugito <raosahebdhotreedu@gmail.com>
2021-09-03 17:31:32 +0530

    added parameterised constructor and update method

--- AddressBook/src/main/java/com/bridgelabz/addressbook/model/AddressBookData.java
+++ AddressBook/src/main/java/com/bridgelabz/addressbook/model/AddressBookData.java
@@ -1,17 +1,42 @@
 package com.bridgelabz.addressbook.model;
 
+import com.bridgelabz.addressbook.dto.AddressBookDTO;
+
 import lombok.Data;
 
 public @Data class AddressBookData {
 	
 	private int person_id;
-
 	private String first_name;
 	private String last_name;
 	private String gender;
-	private long phone_no;
+	private String phone_no;
 	private String city;
 	private String state;
 	private String zip_code;
+	
+	public AddressBookData(AddressBookDTO addressBookDTO) {
+		this.updateAddressBookData(addressBookDTO);
 
+	}
+
+	public AddressBookData(int i, AddressBookDTO addressBookDTO) {
+		this.person_id = i;
+		this.updateAddressBookData(addressBookDTO);
+	}
+
+	public void updateAddressBookData(AddressBookDTO addressBookDTO) {
+		
+		this.first_name = addressBookDTO.first_name;
+		this.last_name = addressBookDTO.last_name;
+		this.gender = addressBookDTO.gender;
+		this.phone_no = addressBookDTO.phone_no;
+		this.city = addressBookDTO.city;
+		this.state = addressBookDTO.state;
+		this.zip_code = addressBookDTO.zip_code;
+		
+	}
+	
+	
+	
 }
