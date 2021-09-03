0e93356a23497f06d045eb66cd1f8080b5dd2ca3
Reugito <raosahebdhotreedu@gmail.com>
2021-09-03 17:33:31 +0530

    added body to the each function and written javadocs for each method

--- AddressBook/src/main/java/com/bridgelabz/addressbook/services/AddressBookServices.java
+++ AddressBook/src/main/java/com/bridgelabz/addressbook/services/AddressBookServices.java
@@ -1,5 +1,6 @@
 package com.bridgelabz.addressbook.services;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
@@ -9,29 +10,60 @@ import com.bridgelabz.addressbook.model.AddressBookData;
 
 @Service
 public class AddressBookServices implements IAddressBookServices {
+	
+	List<AddressBookData> addressbookDataList = new ArrayList();
 
+	/**
+	 * @return list of AddressBook details from List 
+	 */
 	@Override
 	public List<AddressBookData> getEmployeePayrollData() {
-		return null;
+		return addressbookDataList;
 	}
 
+	/** 
+	 * accepts the person id
+	 * @param personId
+	 * @return list of AddressBook details from List 
+	 */
 	@Override
 	public AddressBookData getAddressBookDataById(int personId) {
-		return null;
+		return addressbookDataList.get(personId-1);
 	}
-
+	
+	/**
+	 * accepts the person details in the form 
+	 * of AddressBookDTO and stores it in List
+	 * @param addressBookDTO
+	 * @return accepted addressBookData object
+	 */
 	@Override
-	public AddressBookData createAddressBookData(AddressBookDTO AddressBookDTO) {
-		return null;
+	public AddressBookData createAddressBookData(AddressBookDTO addressBookDTO) {
+		AddressBookData addressBookData = new AddressBookData(addressbookDataList.size()+1, addressBookDTO);
+		addressbookDataList.add(addressBookData);
+		return addressBookData;
 	}
-
+	
+	/**
+	 * accepts the person id and new person details in the form 
+	 * of AddressBookDTO and stores it in List
+	 * @param personId
+	 * @param addressBookDTO
+	 * @return updated addressBookData object
+	 */
 	@Override
 	public AddressBookData updateAddressBookData(int personId, AddressBookDTO AddressBookDTO) {
-		return null;
+		AddressBookData addressBookData = this.getAddressBookDataById(personId-1);
+		addressBookData.updateAddressBookData(AddressBookDTO);
+		return addressBookData;
 	}
 
+	/**
+	 * deletes the data that matches the person id from list
+	 * @param personId
+	 */
 	@Override
 	public void deleteAddressBookData(int personId) {
-		
+		addressbookDataList.remove(personId-1);
 	}
 }
