0fbca4c999bf407889e0aa57fefeffd2af203392
Reugito <raosahebdhotreedu@gmail.com>
2021-09-04 13:34:23 +0530

    Defined DB entities

--- AddressBook/src/main/java/com/bridgelabz/addressbook/model/AddressBookData.java
+++ AddressBook/src/main/java/com/bridgelabz/addressbook/model/AddressBookData.java
@@ -1,12 +1,23 @@
 package com.bridgelabz.addressbook.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 import com.bridgelabz.addressbook.dto.AddressBookDTO;
 
 import lombok.Data;
 
+@Entity
+@Table(name = "addressbook")
 public @Data class AddressBookData {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int person_id;
+	
 	private String first_name;
 	private String last_name;
 	private String gender;
@@ -15,16 +26,12 @@ public @Data class AddressBookData {
 	private String state;
 	private String zip_code;
 	
+	public AddressBookData() {}
+	
 	public AddressBookData(AddressBookDTO addressBookDTO) {
 		this.updateAddressBookData(addressBookDTO);
 
 	}
-
-	public AddressBookData(int i, AddressBookDTO addressBookDTO) {
-		this.person_id = i;
-		this.updateAddressBookData(addressBookDTO);
-	}
-
 	public void updateAddressBookData(AddressBookDTO addressBookDTO) {
 		
 		this.first_name = addressBookDTO.first_name;
@@ -36,7 +43,4 @@ public @Data class AddressBookData {
 		this.zip_code = addressBookDTO.zip_code;
 		
 	}
-	
-	
-	
 }
