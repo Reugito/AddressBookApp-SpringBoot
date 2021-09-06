3c54a514781d09f999c241312d45c1585acaed61
Reugito <raosahebdhotreedu@gmail.com>
2021-09-06 10:20:54 +0530

    added message configuration file

---
+++ AddressBook/src/main/java/com/bridgelabz/addressbook/config/AppConfiguration.java
@@ -0,0 +1,28 @@
+package com.bridgelabz.addressbook.config;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+
+@Configuration
+public class AppConfiguration {
+
+	@Bean
+	public MessageSource messageSource() {
+		ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+		messageSource.setBasenames("classpath:/messages/api_error_messages",
+				"classpath:/messages/api_response_messages");
+		messageSource.setDefaultEncoding("UTF-8");
+		return messageSource;
+	}
+
+	@Bean
+	public LocalValidatorFactoryBean validator(MessageSource messageSource) {
+		LocalValidatorFactoryBean bean = new LocalValidatorFactoryBean();
+		bean.setValidationMessageSource(messageSource);
+		return bean;
+	}
+
+}
